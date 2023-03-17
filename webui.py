3125bd8ccf43ef181b528c987a5cfcff93f43819
FaisalAlsawadi <116385874+FaisalAlsawadi@users.noreply.github.com>
2023-03-17 23:53:12 +0300

    Add files via upload

---
+++ webui.py
@@ -0,0 +1,56 @@
+import gradio as gr
+import whisper
+import langs
+import requests
+import json
+
+lang_list = sorted(langs.LANGUAGES.values())
+
+def transcribe(task, device, language, model_size, mic, file):
+        if device =='gpu':
+            device = 'cuda'
+        args = {'task': task}
+        if (model_size == 'tiny.en') or (model_size == 'base.en') or (model_size == 'small.en') or (model_size == 'medium.en'):
+            args['language'] = 'english'
+        elif (language == 'Detect'):
+            args['language'] = None
+        else:
+            args['language'] = language
+        model = whisper.load_model(model_size, device)
+        if mic is not None:
+            audio = mic
+        elif file is not None:
+            audio = file
+        else:
+            return "You must either provide a mic recording or a file"
+        trascript = model.transcribe(audio, **args)
+        url = "https://experimental.willow.vectara.io/v1/completions"
+
+        payload = json.dumps({
+        "model": "text-davinci-003",
+        "prompt": f"Summarize this : {trascript}",
+        "max_tokens": 3000,
+        "temperature": 0})
+        headers = {
+        'Content-Type': 'application/json',
+        'customer-id': '2657721878',
+        'x-api-key': 'zqt_nmmeFnHLsvtJFg0fqsSHdDCgBtKOj6fnREl1Tg'}
+        summary = requests.request("POST", url, headers=headers, data=payload)
+        tmry = summary.json()
+        text_value = tmry
+        return {"text":tmry['summary']['choices'][0]['text']
+}
+
+
+
+demo = gr.Interface(transcribe, 
+    inputs=[
+        gr.Radio(['transcribe', 'translate'], label= 'Task'),
+        gr.Radio(['gpu', 'cpu'], label= 'Device'),
+        gr.Dropdown(lang_list, value='Detect',  label='Audio Language'),
+        gr.Dropdown(['tiny', 'tiny.en', 'base', 'base.en', 'small', 'small.en', 'medium', 'medium.en', 'large'], value='small', label='Model Size'), 
+        gr.Audio(label='Microphone Recording', source='microphone', type='filepath'), 
+        gr.Audio(source='upload', type='filepath', optional=True, label='Audio File')
+        ], 
+    outputs="text")
+demo.launch() 
